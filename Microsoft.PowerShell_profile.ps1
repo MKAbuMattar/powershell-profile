11d88ee24162f18acb772475fcf9b48076dc59b9
MKAbuMattar <mohammad.khaled@outlook.com>
2024-05-08 23:07:10 +0300

    chore: Update PowerShell profile with console encoding and profile update check

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -142,17 +142,6 @@ function Update-PowerShell {
 Invoke-Command -ScriptBlock ${function:Update-PowerShell} -ErrorAction SilentlyContinue
 
 #------------------------------------------------------
-# Check if the user is an administrator
-#------------------------------------------------------
-$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-function prompt {
-  if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
-}
-$adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
-$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
-
-
-#------------------------------------------------------
 # Utility function to check if a command exists
 #------------------------------------------------------
 function Test-CommandExists {
