498ae1d91c7a221d36d7230d6c114bef9eeb71dd
MKAbuMattar <mohammad.khaled@outlook.com>
2024-05-09 01:13:41 +0300

    Refactor PowerShell profile functions, add utility functions for file manipulation and process listing

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -63,9 +63,18 @@ Import-Module -Name Posh-Git
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -PredictionViewStyle InlineView
 
-#------------------------------------------------------
-# Check if Starship is installed
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function invokes the Starship module transiently to load the Starship prompt.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+Invoke-Starship-TransientFunction
+#>
 function Invoke-Starship-TransientFunction {
   &starship module character
 }
@@ -89,9 +98,18 @@ if (Test-Path $ChocolateyProfile) {
   Import-Module $ChocolateyProfile
 }
 
-#------------------------------------------------------
-# Check for Profile updates
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function checks for updates to the PowerShell profile from the GitHub repository specified in the script. If updates are found, it copies the updated profile to the current user's profile path.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+Update-Profile
+#>
 function Update-Profile {
   if (-not $global:canConnectToGitHub) {
     Write-Host "Skipping profile update check due to GitHub.com not responding within 1 second." -ForegroundColor Yellow
@@ -121,9 +139,18 @@ function Update-Profile {
 #------------------------------------------------------
 Invoke-Command -ScriptBlock ${function:Update-Profile} -ErrorAction SilentlyContinue
 
-#------------------------------------------------------
-# Check for PowerShell updates
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function checks for updates to PowerShell. If updates are found, it upgrades PowerShell to the latest version.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+Update-PowerShell
+#>
 function Update-PowerShell {
   if (-not $global:canConnectToGitHub) {
     Write-Host "Skipping PowerShell update check due to GitHub.com not responding within 1 second." -ForegroundColor Yellow
@@ -160,9 +187,19 @@ function Update-PowerShell {
 #------------------------------------------------------
 Invoke-Command -ScriptBlock ${function:Update-PowerShell} -ErrorAction SilentlyContinue
 
-#------------------------------------------------------
-# Utility function to check if a command exists
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function checks if a command exists in the current environment.
+
+.PARAMETER command
+The command to check.
+
+.OUTPUTS
+$exists: True if the command exists, false otherwise.
+
+.EXAMPLE
+Test-CommandExists "ls"
+#>
 function Test-CommandExists {
   param($command)
   $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
@@ -182,9 +219,18 @@ else { 'notepad' }
 #------------------------------------------------------
 Set-Alias -Name vim -Value $EDITOR
 
-#------------------------------------------------------
-# Edit the profile
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function opens the current user's profile in the editor specified by the $EDITOR variable.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+Edit-Profile
+#>
 function Edit-Profile {
   vim $PROFILE.CurrentUserAllHosts
 }
@@ -193,25 +239,54 @@ function Edit-Profile {
 # Utility Functions
 ######################################################
 
-#------------------------------------------------------
-# Utility function to Create a new empty file
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function creates a new empty file with the specified name.
+
+.PARAMETER file
+The file to create.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+touch "file.txt"
+#>
 function touch($file) {
   "" | Out-File $file -Encoding ASCII
 }
 
-#------------------------------------------------------
-# Utility function to find a file
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function finds a file by name.
+
+.PARAMETER name
+The name of the file to find.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+ff "file.txt"
+#>
 function ff($name) {
   Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
     Write-Output "$($_.directory)\$($_)"
   }
 }
 
-#------------------------------------------------------
-# Utility function to get the uptime
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function gets the system uptime in a human-readable format.
+
+.PARAMETER None
+
+.OUTPUTS
+The system uptime in a human-readable format.
+
+.EXAMPLE
+uptime
+#>
 function uptime {
   if ($PSVersionTable.PSVersion.Major -eq 5) {
     Get-WmiObject win32_operatingsystem | Select-Object @{Name = 'LastBootUpTime'; Expression = { $_.ConverttoDateTime($_.lastbootuptime) } } | Format-Table -HideTableHeaders
@@ -221,25 +296,57 @@ function uptime {
   }
 }
 
-#------------------------------------------------------
-# Utility function to reload the profile
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function reloads the profile to apply changes.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+reload-profile
+#>
 function reload-profile {
   & $profile
 }
 
-#------------------------------------------------------
-# Utility function to unzip a file
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function extracts a file to the current directory.
+
+.PARAMETER file
+The file to extract.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+unzip "file.zip"
+#>
 function unzip ($file) {
   Write-Output("Extracting", $file, "to", $pwd)
   $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
   Expand-Archive -Path $fullFile -DestinationPath $pwd
 }
 
-#------------------------------------------------------
-# Utility function to search for a string in a file
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function searches for a string in a file and returns the line that contains the string.
+
+.PARAMETER regex
+The regular expression to search for.
+
+.PARAMETER dir
+The directory to search in.
+
+.OUTPUTS
+The lines that match the regular expression.
+
+.EXAMPLE
+grep "pattern" "file.txt"
+#>
 function grep($regex, $dir) {
   if ( $dir ) {
     Get-ChildItem $dir | select-string $regex
@@ -251,118 +358,291 @@ function grep($regex, $dir) {
 #------------------------------------------------------
 # Utility function to get the volume information
 #------------------------------------------------------
+<#
+.DESCRIPTION
+This function gets the volume information.
+
+.PARAMETER None
+
+.OUTPUTS
+The volume information.
+
+.EXAMPLE
+df
+#>
 function df {
   get-volume
 }
 
-#------------------------------------------------------
-# Utility function to search for a string in a file
-#------------------------------------------------------
+
+<#
+.DESCRIPTION
+This function searches for a string in a file and replaces it with another string.
+
+.PARAMETER file
+The file to search in.
+
+.PARAMETER find
+The string to search for.
+
+.PARAMETER replace
+The string to replace the found string with.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+sed "file.txt" "pattern" "replacement"
+#>
 function sed($file, $find, $replace) {
   (Get-Content $file).replace("$find", $replace) | Set-Content $file
 }
 
-#------------------------------------------------------
-# Utility function to get the definition of a command
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function gets the definition of a command by name.
+
+.PARAMETER name
+The name of the command.
+
+.OUTPUTS
+The definition of the command.
+
+.EXAMPLE
+which "ls"
+#>
 function which($name) {
   Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
-#------------------------------------------------------
-# Utility function to export an environment variable
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function exports an environment variable with the specified name and value.
+
+.PARAMETER name
+The name of the environment variable.
+
+.PARAMETER value
+The value of the environment variable.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+export "name" "value"
+#>
 function export($name, $value) {
   set-item -force -path "env:$name" -value $value;
 }
 
-#------------------------------------------------------
-# Utility function to kill a process
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function kills a process by name or ID.
+
+.PARAMETER name
+The name of the process to kill.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+pkill "process"
+#>
 function pkill($name) {
   Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
 }
 
-#------------------------------------------------------
-# Utility function to find a process
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function finds a process by name or ID.
+
+.PARAMETER name
+The name of the process to find.
+
+.OUTPUTS
+The process.
+
+.EXAMPLE
+pgrep "process"
+#>
 function pgrep($name) {
   Get-Process $name
 }
 
-#------------------------------------------------------
-# Utility function to get the top processes
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function gets the top file.
+
+.PARAMETER Path
+The path to the file to get the top content.
+
+.PARAMETER n
+The number of file to get.
+
+.OUTPUTS
+The top file content.
+
+.EXAMPLE
+head "file.txt" 10
+#>
 function head {
   param($Path, $n = 10)
   Get-Content $Path -Head $n
 }
 
-#------------------------------------------------------
-# Utility function to get the bottom processes
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function gets the bottom file content.
+
+.PARAMETER Path
+The path to the file to get the bottom content.
+
+.PARAMETER n
+The number of file to get.
+
+.OUTPUTS
+The bottom file content.
+
+.EXAMPLE
+tail "file.txt" 10
+#>
 function tail {
   param($Path, $n = 10)
   Get-Content $Path -Tail $n
 }
 
-#------------------------------------------------------
-# Utility function to create a new file
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function creates a new file with the specified name.
+
+.PARAMETER name
+The name of the file to create.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+nf "file.txt"
+#>
 function nf { 
   param($name) New-Item -ItemType "file" -Path . -Name $name
 }
 
-#------------------------------------------------------
-# Utility function to create a new directory
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function creates a new directory with the specified name.
+
+.PARAMETER name
+The name of the directory to create.
+
+.OUTPUTS
+None
+
+.EXAMPLE
+mkdir "directory"
+#>
 function mkcd { 
   param($dir) mkdir $dir -Force; Set-Location $dir
 }
 
-#------------------------------------------------------
-# Utility function Search through the command history
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function searches through the command history.
+
+.PARAMETER searchTerm
+The term to search for in the command history.
+
+.OUTPUTS
+The command history.
+
+.EXAMPLE
+history "command"
+#>
 function history {
-  param($searchTerm) Get-Content (Get-PSReadlineOption).HistorySavePath | Where-Object { $_ -like "*$searchTerm*" } 
+  Get-Content (Get-PSReadlineOption).HistorySavePath | Where-Object { $_ -like "*$args[0]*" } 
 }
 
 ######################################################
 # Navigation Shortcuts
 ######################################################
 
-#------------------------------------------------------
-# Go to the Root directory
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function goes to the root directory.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+root
+#>
 function root {
   Set-Location -Path $HOME
 }
 
-#------------------------------------------------------
-# Go to the Documents directory
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function goes to the Documents directory.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+doc
+#>
 function doc {
   Set-Location -Path $HOME\Documents
 }
 
-#------------------------------------------------------
-# Go to the Downloads directory
-#------------------------------------------------------
-function dl {
+<#
+.DESCRIPTION
+This function goes to the Downloads directory.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+dl
+#>
+function downl {
   Set-Location -Path $HOME\Downloads
 }
 
-#------------------------------------------------------
-# Go to the Desktop directory
-#------------------------------------------------------
-function dt {
+<#
+.DESCRIPTION
+This function goes to the Desktop directory.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+dtop
+#>
+function dtop {
   Set-Location -Path $HOME\Desktop
 }
 
-#------------------------------------------------------
-# Go to the D:\ directory
-#------------------------------------------------------
-function d {
+<#
+.DESCRIPTION
+This function goes to the D:\ directory.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+dc
+#>
+function dc {
   Set-Location -Path "D:\"
 }
 
@@ -370,45 +650,97 @@ function d {
 # Profile Shortcuts
 ######################################################
 
-#------------------------------------------------------
-# Edit the profile
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function opens the profile in the editor.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+ep
+#>
 function ep { 
   vim $PROFILE
 }
 
-#------------------------------------------------------
-# Get System Information
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function gets the system information.
+
+.PARAMETER None
+
+.OUTPUTS
+The system information.
+
+.EXAMPLE
+sysinfo
+#>
 function sysinfo { 
   Get-ComputerInfo 
 }
 
-#------------------------------------------------------
-# Simplified Process Management
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function kills a process by name or ID.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+k9 "process"
+#>
 function k9 { 
   Stop-Process -Name $args[0] 
 }
 
-#------------------------------------------------------
-# Flush the DNS cache
-#------------------------------------------------------
+<#
+.DESCRIPTION
+This function flushes the DNS cache to resolve DNS issues.
+
+.PARAMETER None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+flushdns
+#>
 function flushdns { 
   Clear-DnsClientCache 
 }
 
+<#
+.SYNOPSIS
+Lists files and directories.
 
-#------------------------------------------------------
-# List all files and directories
-#------------------------------------------------------
-function la {
-  Get-ChildItem -Path . -Force | Format-Table -AutoSize
-}
+.DESCRIPTION
+This function lists files and directories either in the current directory or in the specified path.
 
-#------------------------------------------------------
-# List all files and directories including hidden
-#------------------------------------------------------
+.PARAMETER Path
+Specifies the path of the directory to list. If no path is provided, the current directory is listed.
+
+.OUTPUTS
+The files and directories in the specified path.
+
+.EXAMPLE
+ll
+Lists files and directories in the current directory.
+
+.EXAMPLE
+ll "C:\Users"
+Lists files and directories in the "C:\Users" directory.
+#>
 function ll {
-  Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize 
+  [CmdletBinding()]
+  param (
+    [Parameter(Position = 0)]
+    [string]$Path = "."
+  )
+
+  Get-ChildItem -Path $Path -Force | Format-Table -AutoSize
 }
